30f26afd848ddeb8a569ca0a9994e2af6cfae0d3
eilonben <eilonben@post.bgu.ac.il>
2017-11-14 10:48:50 +0200

    14.11 10:48

---
+++ cmake-build-debug/CMakeFiles/splass1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/studs/bsc/2016/eilonben/CLionProjects/SPL-ass-1/Files.cpp" "/users/studs/bsc/2016/eilonben/CLionProjects/SPL-ass-1/cmake-build-debug/CMakeFiles/splass1.dir/Files.cpp.o"
+  "/users/studs/bsc/2016/eilonben/CLionProjects/SPL-ass-1/Main.cpp" "/users/studs/bsc/2016/eilonben/CLionProjects/SPL-ass-1/cmake-build-debug/CMakeFiles/splass1.dir/Main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/splass1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/splass1.dir/Main.cpp.o"
+  "CMakeFiles/splass1.dir/Files.cpp.o"
+  "splass1.pdb"
+  "splass1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/splass1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
